2d8e705d8331fc73f4601661729423a76bc07a5d
Jason Howell <howell.jasong@gmail.com>
2025-02-18 15:53:32 -0600

    vault workshop

---
+++ vault.js
@@ -0,0 +1,19 @@
+
+//STEPS: 1.Create a string 2.Assign three variable 3.Add comments 4.Create a Dialog box 
+
+
+//Creating a string
+let phrase = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+
+//Assigning 3 variables using three types of an arithmetic operator (mltply, add, subtract)
+let num1 = 5 * 2
+let num2 = 39 + 1
+let num3 = 45 - 6
+
+
+//a console log to confirm phrase equals the written prase
+console.log(phrase);
+
+//using alert to display a dialog box with phrase and variables to reveal combo
+alert(`${phrase} ${num1}-${num2}-${num3}`)
